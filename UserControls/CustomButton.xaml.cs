2f4d7ea2a712ff966f549d555b4476b489170d63
Josephine Johansen <158573410+josephinekmj@users.noreply.github.com>
2024-05-15 09:28:26 +0200

    Usercontrol CustomButton er ikke benyttet
    Knappen kan kaldes i MainWindow ved at bruge using usercontrol.....namespace og så lave content på knappen om vha. Binding. 
    Der mangler eventhandling. click er på en måde reserveret af skabelonen. der er dobbeltklik tilgængelig umiddelbart inde fra mainwindow.xaml når man vil lave event på hver enkelt knap.

---
+++ UserControls/CustomButton.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WindSpareParts.View.UserControls
+{
+    public partial class CustomButton : UserControl, INotifyPropertyChanged
+    {
+        public CustomButton()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        // Dependency property for changing the button's text
+        public static readonly DependencyProperty ButtonTextProperty =
+            DependencyProperty.Register("ButtonText", typeof(string), typeof(CustomButton), new PropertyMetadata(""));
+
+        // Property for accessing the button's text
+        public string ButtonText
+        {
+            get { return (string)GetValue(ButtonTextProperty); }
+            set { SetValue(ButtonTextProperty, value); }
+        }
+
+        // Event to notify property changes
+        public event PropertyChangedEventHandler PropertyChanged;
+
+  
+
+        // Method to raise property changed event
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void btnCustomButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+            var button = (CustomButton)sender;
+
+            
+        }
+    }
+}
