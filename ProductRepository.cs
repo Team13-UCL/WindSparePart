d49af93b2d0ea11e983a91986105e9558d8788a2
Josephine Johansen <158573410+josephinekmj@users.noreply.github.com>
2024-05-15 09:19:55 +0200

    Disse klasser er ikke en del af vores system
    Disse klasser er ikke en del af vores system, men de indeholder interface og usercontrol GUI-elementer, som i fremtiden kunne være smart at benytte. 
    I MVVM er de placeret i Model.

---
+++ ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KindOfAnInventorySystem.Model
+{
+    public class ProductRepository : IRepository
+
+    {
+        Product product = new Product();
+        private List<Product> products;
+
+        void IRepository.Create()
+        {
+            products.Add(product);
+        }
+
+           
+
+        void IRepository.Delete()
+        {
+            products.Remove(product);
+        }
+
+        void IRepository.Read()
+        {
+            
+        }
+
+        void IRepository.Search()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository.Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
